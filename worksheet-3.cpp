d1375a74e0b819d5c05129d2dc87112144fc5504
billymosissa <89740343+billymosissa@users.noreply.github.com>
2023-01-17 04:02:18 -0500

    Update worksheet-3.cpp

--- worksheet-3.cpp
+++ worksheet-3.cpp
@@ -1,1 +1,16 @@
-hello
+#cool
+
+#2
+#include <iostream>
+
+int main()
+{
+int num = 10;
+    for (int a = 0; a < 4; a++) {
+        for (int i = 0; i < 10; i++, num++) {
+            std::cout << num << " ";
+        }
+        std::cout<< std::endl;
+}
+    return 0;
+}
